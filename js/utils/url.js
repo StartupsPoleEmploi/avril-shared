bca22ac71fcc49bf59e4517b49e5d09614413110
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-04-23 11:16:57 +0200

    refacto

--- js/utils/url.js
+++
@@ -1,3 +0,0 @@
-export const objectToQueryString = object => Object.keys(object).filter(k => object[k]).map(k => {
-  return `${encodeURIComponent(k)}=${encodeURIComponent(object[k])}`
-}).join('&');
