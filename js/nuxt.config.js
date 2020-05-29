7e16743a92ab4beb6478c3615250ff129153d945
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-05-29 16:18:58 +0200

    plugins shared

--- js/nuxt.config.js
+++ js/nuxt.config.js
@@ -1,5 +1,81 @@
-module.exports = {
+export default {
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+  },
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    loaders: {
+      scss: { sourceMap: false },
+    },
+    extend (config, ctx) {}
+  },
+  buildDir: process.env.NUXT_BUILD_DIR,
+  env: {
+    clientToPhoenixUrl: process.env.CLIENT_TO_PHOENIX_URL,
+    serverToPhoenixUrl: process.env.SERVER_TO_PHOENIX_URL,
+    hotjarId: process.env.NUXT_HOTJAR_ID,
+    crispWebsiteId: process.env.NUXT_CRISP_WEBSITE_ID,
+    serverAuthKey: process.env.SECRET_KEY_BASE,
+    algoliaCredentials: {
+      id: process.env.NUXT_ALGOLIA_PLACES_APP_ID,
+      key: process.env.NUXT_ALGOLIA_PLACES_API_KEY
+    },
+  },
   head: {
-    title: 'Super shared title',
-  }
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/images/favicons/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/images/favicons/apple-icon-57x57.png'},
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/images/favicons/apple-icon-60x60.png'},
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/images/favicons/apple-icon-72x72.png'},
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/images/favicons/apple-icon-76x76.png'},
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/images/favicons/apple-icon-114x114.png'},
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/images/favicons/apple-icon-120x120.png'},
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/images/favicons/apple-icon-144x144.png'},
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/images/favicons/apple-icon-152x152.png'},
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/favicons/apple-icon-180x180.png'},
+      { rel: 'icon', type: 'image/png', sizes: '192x192',  href: '/images/favicons/android-icon-192x192.png'},
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/favicons/favicon-32x32.png'},
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/images/favicons/favicon-96x96.png'},
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/images/favicons/favicon-16x16.png'},
+    ],
+  },
+  mode: 'universal',
+  modules: [
+    'nuxt-svg-loader',
+    '@nuxtjs/markdownit',
+    '@nuxtjs/google-analytics',
+    '@nuxtjs/sentry',
+    'cookie-universal-nuxt',
+  ],
+  plugins: [
+    'avril/js/plugins/filters.js',
+    { src: 'avril/js/plugins/datepicker', mode: 'client' },
+    'avril/js/plugins/hotjar.js',
+    'avril/js/plugins/crisp.js',
+  ],
+  googleAnalytics: {
+    id: [
+      process.env.GA_API_KEY,
+      process.env.GA_PE_API_KEY,
+    ].filter(v => v),
+    dev: false,
+  },
+  sentry: {
+    config: {
+      environment: process.env.NODE_ENV,
+      release: process.env.HEROKU_SLUG_COMMIT,
+    },
+  },
 }
---
+++ js/plugins/datepicker.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import DatePicker from 'vue2-datepicker'
+import 'vue2-datepicker/locale/fr';
+
+Vue.component('date-picker', DatePicker)
