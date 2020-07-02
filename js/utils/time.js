05b8a1063a5feee627917f76b7e8eea1ea801dc9
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-07-02 13:03:35 +0200

    update weekHours

--- js/utils/time.js
+++ js/utils/time.js
@@ -15,7 +15,7 @@ export const workedDays = period => {
 
 export const periodTotalHours = period => {
   if (period.totalHours) return period.totalHours;
-  const dailyHours = parseInt(period.weekHours)/5;
+  const dailyHours = parseInt(period.weekHoursDuration)/5;
   return parseInt(dailyHours * workedDays(period))
 }
 
