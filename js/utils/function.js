5fbf174faad9359dc0e848473c16b73cdf873526
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-12-14 22:13:12 +0100

    debounce added

--- js/utils/function.js
+++ js/utils/function.js
@@ -2,3 +2,18 @@ export const labelGetter = (array, value) => {
   const answer =array.find(a => a.value === value)
     if (answer) return answer.label;
 }
+
+export const debounce = (func, wait, immediate) => {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
