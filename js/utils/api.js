459bec36125f7a35d142a4610efdfa8fd1265fa3
Augustin Riedinger <augustin.riedinger@gmail.com>
2022-03-22 13:13:47 +0100

    fix?

--- js/utils/api.js
+++ js/utils/api.js
@@ -37,7 +37,7 @@ const universalFetch = context => async (url, options) => {
   if (process.client) {
     return window.fetch(url, options);
   } else {
-    const {default: fetch} = await import('node-fetch');
+    const {default: fetch} = await import('@/node_modules/node-fetch/src/index.js');
     // const fetch = require('node-fetch');
     const {
       env: {
