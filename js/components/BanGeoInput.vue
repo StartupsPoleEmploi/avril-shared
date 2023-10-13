3ca38fdd07a005a5383327ff7f8e0d2fa5f6c65b
Augustin Riedinger <augustin.riedinger@gmail.com>
2023-10-13 12:00:55 +0200

    upgrade sahred

--- js/components/BanGeoInput.vue
+++ js/components/BanGeoInput.vue
@@ -148,7 +148,6 @@
       },
       search: async function(input) {
         try {
-          console.log({input})
           if (input && input.replace(/[/s]/g, '').length >= 3) {
             this.isLoading = true;
             const banFilter = geoTypeToBanType(this.type);
